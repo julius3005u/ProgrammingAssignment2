2c6c116bc29b80d73077b3f5a41b403bf8aaf5e7
julius3005u <69476893+julius3005u@users.noreply.github.com>
2020-08-10 15:05:14 -0500

    Coding assigment, using examples. Only changing things appropriately.

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,38 @@
 ## This function creates a special "matrix" object
 ## that can cache its inverse.
-## functions do
 
-## Write a short comment describing this function
+
+## structure is a copy and paste from README.txt
+## changing makeVector function as be neccesary
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL ## the matrix
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setminv <- function(minversa) m <<- minversa
+    getminv <- function() m
+    list(set = set, get = get,
+         setminv = setminv,
+         getminv = getminv)
+  
 }
 
 
-## Write a short comment describing this function
+## Find the inverse of matrix set with makeCacheMatrix
+## changing things appropriately in cachemean
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getminv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setminv(m)
+  m
 }
